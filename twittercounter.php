a4dc52708b6ecb75b9df1ccaeda38631c6ee3dc6
Ajay D'Souza <me@ajaydsouza.com>
2014-07-13 12:45:38 +0100

    Updated WordPress widget

--- twittercounter.php
+++ twittercounter.php
@@ -189,7 +189,7 @@ function tc_default_options() {
 	$tc_settings = 	array (
 		'username' => '',			// Twitter Username
 		'twitter_id' => '',			// twitter id
-		'style' => 'avatar',		// Twitter Counter style
+		'style' => 'custom',		// Twitter Counter style
 		'users_id' => '',			// Twitter Counter userid
 		'a_color' => '709cb2',		// Twitter Widget Text and Hyperlink
 		'hr_color' => '709CB2',		// Twitter Widget Horizontal rules
@@ -258,60 +258,91 @@ function twittercounter_api( $twitter_id= '3412651' ) {
  * @extends WP_Widget
  */
 class WidgetTC extends WP_Widget {
-	function WidgetTC()
-	{
-		$widget_ops = array('classname' => 'widget_twittercounter', 'description' => __( 'Display Twitter Counter', 'twittercounter' ) );
-		$this->WP_Widget('widget_twittercounter',__( 'Twitter Counter', 'twittercounter' ), $widget_ops);
+
+	/**
+	 * Register widget with WordPress.
+	 */
+	function __construct() {
+		parent::__construct(
+			'widget_twittercounter', // Base ID
+			__( 'Twitter Counter', 'twittercounter' ), // Name
+			array( 'description' => __( 'Display Twitter Counter button', 'twittercounter' ), ) // Args
+		);
 	}
-	function form($instance) {
-		$title = isset($instance['title']) ? esc_attr($instance['title']) : '';
-		$style = isset($instance['style']) ? esc_attr($instance['style']) : 'avatar';
+
+	/**
+	 * Back-end widget form.
+	 *
+	 * @see WP_Widget::form()
+	 *
+	 * @param array $instance Previously saved values from database.
+	 */
+	function form( $instance ) {
+		$title = isset( $instance['title'] ) ? esc_attr( $instance['title'] ) : '';
+		$style = isset( $instance['style'] ) ? esc_attr( $instance['style'] ) : 'avatar';
 		?>
 		<p>
-		<label for="<?php echo $this->get_field_id('title'); ?>">
-		<?php _e('Title', 'twittercounter'); ?>: <input class="widefat" id="<?php echo $this->get_field_id('title'); ?>" name="<?php echo $this->get_field_name('title'); ?>" type="text" value="<?php echo esc_attr($title); ?>" />
-		</label>
+			<label for="<?php echo $this->get_field_id( 'title' ); ?>">
+				<?php _e( 'Title', 'twittercounter' ); ?>:
+				<input class="widefat" id="<?php echo $this->get_field_id( 'title' ); ?>" name="<?php echo $this->get_field_name( 'title' ); ?>" type="text" value="<?php echo esc_attr( $title ); ?>" />
+			</label>
 		</p>
 		<p>
-		<?php _e('Style', 'twittercounter'); ?>: <br />
-		<select class="widefat" id="<?php echo $this->get_field_id('style'); ?>" name="<?php echo $this->get_field_name('style'); ?>">
-		  <option value="avatar" <?php if ($style=='avatar') echo 'selected="selected"' ?>>avatar</option>
-		  <option value="bird" <?php if ($style=='bird') echo 'selected="selected"' ?>>bird</option>
-		  <option value="custom" <?php if ($style=='custom') echo 'selected="selected"' ?>>custom</option>
-		  <option value="script_only" <?php if ($style=='script_only') echo 'selected="selected"' ?>>script_only</option>
-		  <option value="blank" <?php if ($style=='blank') echo 'selected="selected"' ?>>blank</option>
-		  <option value="black" <?php if ($style=='black') echo 'selected="selected"' ?>>black</option>
-		  <option value="white" <?php if ($style=='white') echo 'selected="selected"' ?>>white</option>
-		  <option value="blue" <?php if ($style=='blue') echo 'selected="selected"' ?>>blue</option>
-		</select>
+			<?php _e( 'Style', 'twittercounter' ); ?>: <br />
+			<select class="widefat" id="<?php echo $this->get_field_id( 'style' ); ?>" name="<?php echo $this->get_field_name( 'style' ); ?>">
+			  <option value="avatar" <?php if ( 'avatar' == $style ) { echo 'selected="selected"'; } ?>><?php _e( 'Avatar', 'twittercounter' ); ?></option>
+			  <option value="bird" <?php if ( 'bird' == $style ) { echo 'selected="selected"'; } ?>><?php _e( 'Big Bird', 'twittercounter' ); ?></option>
+			  <option value="custom" <?php if ( 'custom' == $style ) { echo 'selected="selected"'; } ?>><?php _e( 'Classic', 'twittercounter' ); ?></option>
+			  <option value="script_only" <?php if ( 'script_only' == $style ) { echo 'selected="selected"'; } ?>><?php _e( 'Count only', 'twittercounter' ); ?></option>
+			</select>
 		</p>
 		<?php
 	} //ending form creation
-	function update($new_instance, $old_instance) {
+
+	/**
+	 * Sanitize widget form values as they are saved.
+	 *
+	 * @see WP_Widget::update()
+	 *
+	 * @param array $new_instance Values just sent to be saved.
+	 * @param array $old_instance Previously saved values from database.
+	 *
+	 * @return array Updated safe values to be saved.
+	 */
+	function update( $new_instance, $old_instance ) {
 		$instance = $old_instance;
-		$instance['title'] = strip_tags($new_instance['title']);
-		$instance['style'] = strip_tags($new_instance['style']);
+		$instance['title'] = strip_tags( $new_instance['title'] );
+		$instance['style'] = strip_tags( $new_instance['style'] );
 		return $instance;
 	} //ending update
-	function widget($args, $instance) {
-		global $wpdb;
 
-		extract($args, EXTR_SKIP);
+	/**
+	 * Front-end display of widget.
+	 *
+	 * @see WP_Widget::widget()
+	 *
+	 * @param array $args     Widget arguments.
+	 * @param array $instance Saved values from database.
+	 */
+	function widget( $args, $instance ) {
+		global $wpdb, $tc_settings;
 
-		global $tc_settings;
+		extract( $args, EXTR_SKIP );
 
-		$title = apply_filters('widget_title', $instance['title']);
+		$title = apply_filters( 'widget_title', $instance['title'] );
 		$style = $instance['style'];
-		if (empty($style)) $style = $tc_settings['style'];
+		if ( empty( $style ) ) {
+			$style = $tc_settings['style'];
+		}
 
 		echo $before_widget;
 		echo $before_title . $title . $after_title;
 
-		echo ald_tc($style);
+		echo ald_tc( $style );
 		echo $after_widget;
 
 	} //ending function widget
-}
+} // End WidgetTC
 
 
 /**
@@ -320,98 +351,128 @@ class WidgetTC extends WP_Widget {
  * @extends WP_Widget
  */
 class WidgetTW extends WP_Widget {
-	function WidgetTW()
-	{
-		$widget_ops = array('classname' => 'widget_twitterwidget', 'description' => __( 'Display Twitter Widget', 'twittercounter' ) );
-		$this->WP_Widget('widget_twitterwidget',__( 'Twitter Widget', 'twittercounter' ), $widget_ops);
+
+	/**
+	 * Register widget with WordPress.
+	 */
+	function __construct() {
+		parent::__construct(
+			'widget_twitterwidget', // Base ID
+			__( 'Twitter Widget', 'twittercounter' ), // Name
+			array( 'description' => __( 'Display Twitter Widget', 'twittercounter' ), ) // Args
+		);
 	}
-	function form($instance) {
-		$title = isset($instance['title']) ? esc_attr($instance['title']) : '';
-		$width = isset($instance['width']) ? esc_attr($instance['width']) : '';
-		$nr_show = isset($instance['nr_show']) ? esc_attr($instance['nr_show']) : '';
-		$hr_color = isset($instance['hr_color']) ? esc_attr($instance['hr_color']) : '';
-		$a_color = isset($instance['a_color']) ? esc_attr($instance['a_color']) : '';
-		$bg_color = isset($instance['bg_color']) ? esc_attr($instance['bg_color']) : '';
+
+	/**
+	 * Back-end widget form.
+	 *
+	 * @see WP_Widget::form()
+	 *
+	 * @param array $instance Previously saved values from database.
+	 */
+	function form( $instance ) {
+		$title = isset( $instance['title'] ) ? esc_attr( $instance['title'] ) : '';
+		$width = isset( $instance['width'] ) ? esc_attr( $instance['width'] ) : '';
+		$nr_show = isset( $instance['nr_show'] ) ? esc_attr( $instance['nr_show'] ) : '';
+		$hr_color = isset( $instance['hr_color'] ) ? esc_attr( $instance['hr_color'] ) : '';
+		$a_color = isset( $instance['a_color'] ) ? esc_attr( $instance['a_color'] ) : '';
+		$bg_color = isset( $instance['bg_color'] ) ? esc_attr( $instance['bg_color'] ) : '';
 		?>
 		<p>
-		<label for="<?php echo $this->get_field_id('title'); ?>">
-		<?php _e('Title', 'twittercounter'); ?>: <input class="widefat" id="<?php echo $this->get_field_id('title'); ?>" name="<?php echo $this->get_field_name('title'); ?>" type="text" value="<?php echo esc_attr($title); ?>" />
-		</label>
+			<label for="<?php echo $this->get_field_id( 'title' ); ?>">
+				<?php _e( 'Title', 'twittercounter' ); ?>:
+				<input class="widefat" id="<?php echo $this->get_field_id( 'title' ); ?>" name="<?php echo $this->get_field_name( 'title' ); ?>" type="text" value="<?php echo esc_attr( $title ); ?>" />
+			</label>
 		</p>
-
 		<p><?php _e('Leave the below options blank to use the default settings', 'twittercounter'); ?></p>
 		<p>
-		<label for="<?php echo $this->get_field_id('width'); ?>">
-		<?php _e('Width', 'twittercounter'); ?>: <input class="widefat" id="<?php echo $this->get_field_id('width'); ?>" name="<?php echo $this->get_field_name('width'); ?>" type="text" value="<?php echo esc_attr($width); ?>" />
-		</label>
+			<label for="<?php echo $this->get_field_id( 'width' ); ?>">
+				<?php _e( 'Width', 'twittercounter' ); ?>:
+				<input class="widefat" id="<?php echo $this->get_field_id( 'width' ); ?>" name="<?php echo $this->get_field_name( 'width' ); ?>" type="text" value="<?php echo esc_attr( $width ); ?>" />
+			</label>
 		</p>
-
 		<p>
-		<label for="<?php echo $this->get_field_id('nr_show'); ?>">
-		<?php _e('Number of rows', 'twittercounter'); ?>: <input class="widefat" id="<?php echo $this->get_field_id('nr_show'); ?>" name="<?php echo $this->get_field_name('nr_show'); ?>" type="text" value="<?php echo esc_attr($nr_show); ?>" />
-		</label>
+			<label for="<?php echo $this->get_field_id( 'nr_show' ); ?>">
+				<?php _e( 'Number of rows', 'twittercounter' ); ?>:
+				<input class="widefat" id="<?php echo $this->get_field_id( 'nr_show' ); ?>" name="<?php echo $this->get_field_name( 'nr_show' ); ?>" type="text" value="<?php echo esc_attr( $nr_show ); ?>" />
+			</label>
 		</p>
-
+		<p><em><?php _e('Enter 6 character hexadecimal code without the # for the below options. e.g. for white, enter FFFFFF', 'twittercounter'); ?></em></p>
 		<p>
-		<label for="<?php echo $this->get_field_id('hr_color'); ?>">
-		<?php _e('Header horizontal rules', 'twittercounter'); ?>: <input class="widefat" id="<?php echo $this->get_field_id('hr_color'); ?>" name="<?php echo $this->get_field_name('hr_color'); ?>" type="text" value="<?php echo esc_attr($hr_color); ?>" />
-		</label>
+			<label for="<?php echo $this->get_field_id( 'hr_color' ); ?>">
+				<?php _e( 'Header Color', 'twittercounter' ); ?>:
+				<input class="widefat" id="<?php echo $this->get_field_id( 'hr_color' ); ?>" name="<?php echo $this->get_field_name( 'hr_color' ); ?>" type="text" value="<?php echo esc_attr( $hr_color ); ?>" />
+			</label>
 		</p>
-
 		<p>
-		<label for="<?php echo $this->get_field_id('a_color'); ?>">
-		<?php _e('Text and links', 'twittercounter'); ?>: <input class="widefat" id="<?php echo $this->get_field_id('a_color'); ?>" name="<?php echo $this->get_field_name('a_color'); ?>" type="text" value="<?php echo esc_attr($a_color); ?>" />
-		</label>
+			<label for="<?php echo $this->get_field_id( 'a_color' ); ?>">
+				<?php _e( 'Text and links Color', 'twittercounter' ); ?>:
+				<input class="widefat" id="<?php echo $this->get_field_id( 'a_color' ); ?>" name="<?php echo $this->get_field_name( 'a_color' ); ?>" type="text" value="<?php echo esc_attr( $a_color ); ?>" />
+			</label>
 		</p>
-
 		<p>
-		<label for="<?php echo $this->get_field_id('bg_color'); ?>">
-		<?php _e('Background Color', 'twittercounter'); ?>: <input class="widefat" id="<?php echo $this->get_field_id('bg_color'); ?>" name="<?php echo $this->get_field_name('bg_color'); ?>" type="text" value="<?php echo esc_attr($bg_color); ?>" />
-		</label>
+			<label for="<?php echo $this->get_field_id( 'bg_color' ); ?>">
+				<?php _e( 'Background Color', 'twittercounter' ); ?>:
+				<input class="widefat" id="<?php echo $this->get_field_id( 'bg_color' ); ?>" name="<?php echo $this->get_field_name( 'bg_color' ); ?>" type="text" value="<?php echo esc_attr( $bg_color ); ?>" />
+			</label>
 		</p>
 
 		<?php
 	} //ending form creation
-	function update($new_instance, $old_instance) {
+
+	/**
+	 * Sanitize widget form values as they are saved.
+	 *
+	 * @see WP_Widget::update()
+	 *
+	 * @param array $new_instance Values just sent to be saved.
+	 * @param array $old_instance Previously saved values from database.
+	 *
+	 * @return array Updated safe values to be saved.
+	 */
+	function update( $new_instance, $old_instance ) {
 		$instance = $old_instance;
-		$instance['title'] = strip_tags($new_instance['title']);
-		$instance['width'] = strip_tags($new_instance['width']);
-		$instance['nr_show'] = strip_tags($new_instance['nr_show']);
-		$instance['hr_color'] = strip_tags($new_instance['hr_color']);
-		$instance['a_color'] = strip_tags($new_instance['a_color']);
-		$instance['bg_color'] = strip_tags($new_instance['bg_color']);
+		$instance['title'] = strip_tags( $new_instance['title'] );
+		$instance['width'] = strip_tags( $new_instance['width'] );
+		$instance['nr_show'] = strip_tags( $new_instance['nr_show'] );
+		$instance['hr_color'] = strip_tags( $new_instance['hr_color'] );
+		$instance['a_color'] = strip_tags( $new_instance['a_color'] );
+		$instance['bg_color'] = strip_tags( $new_instance['bg_color'] );
 		return $instance;
 	} //ending update
-	function widget($args, $instance) {
-		global $wpdb;
+
+	/**
+	 * Front-end display of widget.
+	 *
+	 * @see WP_Widget::widget()
+	 *
+	 * @param array $args     Widget arguments.
+	 * @param array $instance Saved values from database.
+	 */
+	function widget( $args, $instance ) {
+		global $wpdb, $tc_settings;
 
 		extract($args, EXTR_SKIP);
 
-		global $tc_settings;
+		$title = apply_filters( 'widget_title', $instance['title'] );
 
-		$title = apply_filters('widget_title', $instance['title']);
-		$style = $instance['style'];
-		if (empty($style)) $style = $tc_settings['style'];
-
-		$width = $instance['width'];
-		if (empty($width)) $width = $tc_settings['width'];
-
-		$nr_show = $instance['nr_show'];
-		if (empty($nr_show)) $nr_show = $tc_settings['nr_show'];
-
-		$hr_color = $instance['hr_color'];
-		if (empty($hr_color)) $hr_color = $tc_settings['hr_color'];
-
-		$a_color = $instance['a_color'];
-		if (empty($a_color)) $a_color = $tc_settings['a_color'];
-
-		$bg_color = $instance['bg_color'];
-		if (empty($bg_color)) $bg_color = $tc_settings['bg_color'];
+		$style = empty( $instance['style'] ) ? $tc_settings['style'] : $instance['style'];
+		$width = empty( $instance['width'] ) ? $tc_settings['width'] : intval( $instance['width'] );
+		$nr_show = empty( $instance['nr_show'] ) ? $tc_settings['nr_show'] : intval( $instance['nr_show'] );
+		$hr_color = empty( $instance['hr_color'] ) ? $tc_settings['hr_color'] : $instance['hr_color'];
+		$a_color = empty( $instance['a_color'] ) ? $tc_settings['a_color'] : $instance['a_color'];
+		$bg_color = empty( $instance['bg_color'] ) ? $tc_settings['bg_color'] : $instance['bg_color'];
 
 		echo $before_widget;
 		echo $before_title . $title . $after_title;
 
-		echo ald_tr($width, $nr_show, $hr_color, $a_color, $bg_color);
+		echo ald_tr( array(
+			'width' => $width,
+			'nr_show' => $nr_show,
+			'hr_color' => $hr_color,
+			'a_color' => $a_color,
+			'bg_color' => $bg_color,
+		) );
 		echo $after_widget;
 
 	} //ending function widget
@@ -421,11 +482,12 @@ class WidgetTW extends WP_Widget {
 /**
  * Initialise the widgets.
  */
-function init_ald_tc(){
-	register_widget('WidgetTC');
-	register_widget('WidgetTW');
+function register_tc_widgets() {
+	register_widget( 'WidgetTC' );
+	register_widget( 'WidgetTW' );
 }
-add_action('widgets_init', 'init_ald_tc', 1);
+add_action( 'widgets_init', 'register_tc_widgets' );
+
 
 /**
  *  Admin options
